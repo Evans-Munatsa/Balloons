cd79b495b0cefef9ae74e596be1e5fa092329a1e
Evans-Munatsa <gagamel.em@gmail.com>
2016-04-06 14:28:14 +0200

    added price

--- balloon.js
+++ balloon.js
@@ -1,30 +1,36 @@
 var balloons = "4 sets of red, 3 sets of blue, 3 sets of yellow.";
-var prices = "R4 for red, R5 for blue, and R5.50 for yellow.";
 
-var split = balloons.replace(" and ", " ").split(",");
-var objects =[];
+var split = balloons.replace(" and  ", " ").split(",");
+var objects = [];
 
-
-var cut = prices.replace(" and ", "").split(",");
-
-split.forEach(function(balloons){
-  objects.push(balloons.split(" sets of "));
+split.forEach(function(balloons) {
+    objects.push(balloons.split(" sets of "));
 });
+console.log(objects);
 
-var powers = [];
-cut.forEach(function(prices){
-  powers.push(prices.split(" for"));
-})
-
-// var balloon_set = 3;
 var amount = [];
 
-objects.forEach(function(balloons){
-  var sets = 3;
-  var object = balloons[0];
-  var answer = sets * object;
-  amount.push(answer);
+objects.forEach(function(balloons) {
+    var sets = 3;
+    var object = balloons[0];
+    var answer = sets * object;
+    amount.push(answer);
 });
- console.log(objects);
- console.log(amount);
- console.log(powers);
+console.log(amount);
+
+var prices = "R4 for red, R5 for blue, and R5.50 for yellow.";
+
+var cut = prices.replace(" and", "").replace(/R/g, "").split(",");
+
+var powers = [];
+cut.forEach(function(prices) {
+    powers.push(prices.split(" for"));
+});
+console.log(powers);
+
+var total = [];
+powers.forEach(function(prices){
+  var one = prices[0] * amount[1];
+  total.push(one);
+});
+console.log(total)
